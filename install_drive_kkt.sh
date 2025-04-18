329dbcd020e101ac2cb26b62d9f8186c173894a4
Walsmit <walsmit@mail.ru>
2025-04-18 12:17:11 +0000

    Add new script

---
+++ install_drive_kkt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Проверка параметра
+if [ -z "$1" ]; then
+	echo "Ошибка: неуказан параметр"
+	exit 1
+fi
+
+sudo apt update
+
+unzip $1 $pwd
+
+# Переменая-обрез строки
+short_name=${1%%.zip}
+
+# Установка пакетов
+
+sudo dpkg -i $short_name/installer/deb/libfptr10-gui_"$short_name"_amd64.deb
+sudo dpkg -i $short_name/installer/deb/libfptr10_"$short_name"_amd64.deb
+sudo dpkg -i $short_name/installer/deb/fptr10-test-util_"$short_name"_amd64.deb
+
+# Функция удаление установочных файлов
+func_delete_old_file()
+{
+        echo "Происходит удаление файла $1"
+        sleep 5
+        sudo rm -rf $1
+        echo "Файл: $1 удален."
+}
+
+func_delete_old_file "$1"
+func_delete_old_file "$short_name"
+
+`fptr10_t`
